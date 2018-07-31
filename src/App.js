e26bd1d934095676d2b76a698c2b6ba0be5680c8
Anna Shvets <annashvets11@gmail.com>
2018-08-01 00:04:06 +0200

    feat: Made a starter from npm

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Map from './Map'
 
-
-  class App extends Component {
-    state = {
-
-    }
+class App extends Component {
 
     componentDidMount() {
-      loadMapJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyB1ijS6t6OKocfsfAdu9Nuawo1NfwzJLHQ&v=3&callback=initMap')
+      initMap('https://maps.googleapis.com/maps/api/js?key=AIzaSyB1ijS6t6OKocfsfAdu9Nuawo1NfwzJLHQ&v=3&callback=initMap')
     }
 
-  render(){
-    return(
+  
+
+  render() {
+    return (
       <Map />
-    )
+    );
   }
 }
 
--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 
 class Map extends Component {
 
+  initMap = () => {
+      const map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.7413549, lng: -73.9980244},
+          zoom: 13
+        });
+      }
+
   render(){
     return(
       <div id="map"></div>
-    )
+    );
   }
 }
 
